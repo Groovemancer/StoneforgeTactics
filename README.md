e378d8f7f46d422dc8c1a1eb5a25f57ad223dd41
Tim Downing <icedmetal57@gmail.com>
2014-04-28 18:02:22 -0700

    Initial Commit
    Created project, with the basic setup. Have not yet started coding.

---
+++ README.md
@@ -0,0 +1,3 @@
+Stoneforge Tactics
+==============
+Stoneforge Tactics is a turn-based tactics game developed in Java with the help of LibGDX.
---
+++ android/src/com/pixelatedgravity/stoneforgetactics/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pixelatedgravity.stoneforgetactics.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pixelatedgravity.stoneforgetactics.StoneForgeTactics;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StoneForgeTactics(), config);
+	}
+}
